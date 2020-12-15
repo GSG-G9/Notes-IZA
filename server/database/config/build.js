42d1e52b85fb1eaeb76fbbceb6fd766b57c18dd4
Ahmed-aed <aeid767@gmail.com>
2020-12-15 23:58:47 +0200

    make test for getNotes query related #22

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -5,7 +5,10 @@ const connection = require('./connection');
 
 const dbBuild = () => {
   const sql = readFileSync(join(__dirname, 'note.sql')).toString();
-  return connection.query(sql);
+  return connection.query(sql).then(()=>{
+    const fakeData=readFileSync(join(__dirname, 'fakeNote.sql')).toString();
+    return connection.query(fakeData)
+  })
 };
 
 module.exports = { dbBuild };
