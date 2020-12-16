749348d79faaf8aa88b559c1b9fad29f35616643
Israa91 <eng.israa.1991@gmail.com>
2020-12-16 09:02:00 +0200

    addNote route

--- server/app.js
+++ server/app.js
@@ -10,7 +10,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(router);
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.status(404).send(path.join(__dirname, '..', 'public', '404.html'));
 });
